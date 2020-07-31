08dcb0fb7df93d0493d21ac2559325789543ce52
Krishna Kaladhar <gopukala611@gmail.com>
2020-07-31 08:04:59 -0400

    First Commit

---
+++ readme.txt
@@ -0,0 +1,5 @@
+First download the Entire Folder
+
+Run npm init in terminal
+    It will download all the required modules for this project that are mentioned in package.json file.
+    If nodemon is also required download it 
---
+++ routes/aliens.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+const Alien = require("../models/alien");
+
+router.get("/", async (req, res) => {
+  try {
+    const aliens = await Alien.find();
+    res.json(aliens);
+  } catch (err) {
+    res.send("Error... in getting data " + err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const alien = new Alien({
+    name: req.body.name,
+    tech: req.body.tech,
+    sub: req.body.sub,
+  });
+
+  try {
+    const a1 = await alien.save();
+    res.json(a1);
+  } catch (err) {
+    res.send("Error... in saving data " + err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const alien = await Alien.findById(req.params.id);
+    res.json(alien);
+  } catch (err) {
+    res.send("Error... in getting data of particular Id " + err);
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const alien = await Alien.findById(req.params.id);
+    alien.sub = req.body.sub;
+    const a1 = await alien.save();
+    res.json(a1);
+  } catch (err) {
+    res.send("Error... in patch data of particular Id " + err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const alien = await Alien.findByIdAndDelete(req.params.id);
+    res.json(alien);
+  } catch (err) {
+    res.send("Error... in patch data of particular Id " + err);
+  }
+});
+
+module.exports = router;
